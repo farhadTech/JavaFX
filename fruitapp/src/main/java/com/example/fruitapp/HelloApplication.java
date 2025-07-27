18cefda20cc70f6856a78759992b771e46531bc2
Farhadur Rahman <farhad.rahmandev@gmail.com>
2025-07-28 00:52:39 +0600

    checkboxes - part 1

--- fruitapp/src/main/java/com/example/fruitapp/HelloApplication.java
+++ fruitapp/src/main/java/com/example/fruitapp/HelloApplication.java
@@ -2,19 +2,54 @@ package com.example.fruitapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private Label title;
+    private Label response;
+    private Label selected;
+
+    private CheckBox bananaCB;
+    private CheckBox mangoCB;
+    private CheckBox papayaCB;
+    private CheckBox grapefruitCB;
+    private String fruits;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Favourite Fruit App");
+        title = new Label("What fruits do you like?");
+        response = new Label("");
+        selected = new Label("");
+
+        bananaCB = new CheckBox("Banana");
+        mangoCB = new CheckBox("Mango");
+        papayaCB = new CheckBox("Papaya");
+        grapefruitCB = new CheckBox("Grapefruit");
+
+        // We add all of our controls
+        FlowPane flowPaneRoot = new FlowPane(Orientation.VERTICAL, 5, 5);
+        flowPaneRoot.setAlignment(Pos.CENTER);
+        flowPaneRoot.getChildren().add(title);
+        flowPaneRoot.getChildren().addAll(bananaCB, mangoCB, papayaCB, grapefruitCB, response, selected);
+        showAll();
+
+        // Set up the stage and scene
+        Scene scene = new Scene(flowPaneRoot, 400, 250);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void showAll() {
+        fruits = "";
     }
 
     public static void main(String[] args) {
