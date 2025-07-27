66e1eda02d36d44720d0c55b13a4ea2939bff530
Farhadur Rahman <farhad.rahmandev@gmail.com>
2025-07-27 23:02:58 +0600

    Label and FlowPane

--- javafx/src/main/java/com/example/javafx/HelloApplication.java
+++ javafx/src/main/java/com/example/javafx/HelloApplication.java
@@ -4,19 +4,25 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private Label myLabel;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Button button = new Button();
         button.setText("Click Me!");
+
         button.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent event) {
@@ -24,9 +30,15 @@ public class HelloApplication extends Application {
             }
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
-        Scene scene = new Scene(root, 250, 200);
+        myLabel = new Label();
+        myLabel.setText("This is my label");
+
+//        StackPane root = new StackPane();
+        FlowPane flowPaneRoot = new FlowPane(10, 10);
+        flowPaneRoot.getChildren().add(button);
+        flowPaneRoot.getChildren().add(myLabel);
+        flowPaneRoot.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(flowPaneRoot, 250, 200);
         stage.setScene(scene);
         stage.show();
     }
