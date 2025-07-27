6dd6113abb78e5770a14c4087d6c37d1540cd473
Farhadur Rahman <farhad.rahmandev@gmail.com>
2025-07-28 02:23:37 +0600

    checkboxes - part 2

--- fruitapp/src/main/java/com/example/fruitapp/HelloApplication.java
+++ fruitapp/src/main/java/com/example/fruitapp/HelloApplication.java
@@ -1,6 +1,8 @@
 package com.example.fruitapp;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
@@ -12,7 +14,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements EventHandler {
     private Label title;
     private Label response;
     private Label selected;
@@ -42,6 +44,16 @@ public class HelloApplication extends Application {
         flowPaneRoot.getChildren().addAll(bananaCB, mangoCB, papayaCB, grapefruitCB, response, selected);
         showAll();
 
+        // Attach event listeners to our checkboxes
+        // We have 6 checkboxes here that means we have to create 6 event listeners or actions to each checkbox which is totally fine but that's a lot of work and unnecessary work.
+        // We are going to make sure this class have actually implements an action event listener that way we can fetch that action and select which action to take.
+        // Instead of having 6 event listeners or action to each check box we are going to implement just in one handle method.
+        // And within that method the onclick or action method to check which items or which controls was checked.
+        bananaCB.setOnAction(this);
+        mangoCB.setOnAction(this);
+        papayaCB.setOnAction(this);
+        grapefruitCB.setOnAction(this);
+
         // Set up the stage and scene
         Scene scene = new Scene(flowPaneRoot, 400, 250);
         stage.setScene(scene);
@@ -50,6 +62,51 @@ public class HelloApplication extends Application {
 
     public void showAll() {
         fruits = "";
+        if(bananaCB.isSelected()) fruits += " Banana";
+        if(mangoCB.isSelected()) fruits += " Mango";
+        if(papayaCB.isSelected()) fruits += " Papaya";
+        if(grapefruitCB.isSelected()) fruits += " Grapefruit";
+        selected.setText("Fruits selected: " + fruits);
+    }
+
+
+    // this event object that we have been passed will allow us to check which of these have been selected. Using one method we can check all of them.
+    @Override
+    public void handle(Event event) {
+        Object fruitChecked = event.getSource();
+        // getSource() here gives us the object that we have clicked or checked.
+        if(bananaCB.equals(fruitChecked)) {
+            if(bananaCB.isSelected()) {
+                response.setText("Banana selected.");
+            } else {
+                response.setText("Banana cleared!");
+            }
+            showAll();
+        }
+        else if(mangoCB.equals(fruitChecked)) {
+            if(mangoCB.isSelected()) {
+                response.setText("Mango selected.");
+            } else {
+                response.setText("Mango cleared!");
+            }
+            showAll();
+        }
+        else if(papayaCB.equals(fruitChecked)) {
+            if(papayaCB.isSelected()) {
+                response.setText("Papaya selected.");
+            } else {
+                response.setText("Papaya cleared!");
+            }
+            showAll();
+        }
+        else if(grapefruitCB.equals(fruitChecked)) {
+            if(grapefruitCB.isSelected()) {
+                response.setText("Grapefruit selected.");
+            } else {
+                response.setText("Grapefruit cleared!");
+            }
+            showAll();
+        }
     }
 
     public static void main(String[] args) {
