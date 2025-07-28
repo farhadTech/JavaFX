ce205d72b3f6f26f517bda2cc9361932ac48e62f
Farhadur Rahman <farhad.rahmandev@gmail.com>
2025-07-29 02:04:35 +0600

    Scene Builder - Adding Material Design Library to our Project

--- javafxSceneBuilder/src/main/java/com/example/javafxscenebuilder/Controller.java
+++ javafxSceneBuilder/src/main/java/com/example/javafxscenebuilder/Controller.java
@@ -1,5 +1,7 @@
 package com.example.javafxscenebuilder;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -13,19 +15,46 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
+    private JFXTextField textField;
+
+    @FXML
     private Button clickMeButton;
 
     @FXML
-    private Label labelId;
+    private JFXButton materialButton;
+
+    @FXML
+    private Label clickMeLabel;
+
+    @FXML
+    private Label materialButtonLabel;
+
+    @FXML
+    private Label textFieldLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent actionEvent) {
-                System.out.println("Hello JavaFX");
-                labelId.setText("Hey There!");
+                System.out.println("Click Me Button is working");
+                clickMeLabel.setText("Hey There!");
+            }
+        });
+
+        materialButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Material Button is working");
+                materialButtonLabel.setText("Material Button Clicked");
+            }
+        });
+
+        textField.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String textString = textField.getText();
+                textFieldLabel.setText(textString);
             }
         });
     }
--- javafxSceneBuilder/src/main/java/module-info.java
+++ javafxSceneBuilder/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.javafxscenebuilder {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
 
 
     opens com.example.javafxscenebuilder to javafx.fxml;
